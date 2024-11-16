80fc33927fe294ad74cb78ad700fe4eeb63bd4df
Your Name <you@example.com>
2024-11-16 09:47:58 +0530

    swapping

--- test.c
+++ test.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-int main()
-{
-
-        printf("Hello guys ");
-
-        printf ("\n bye guys " );
+int main () 
+{	
+	int x=3, y=5, z;
+        printf("\n %d", z=x );
+	printf("\n %d", x=y);
+	printf("\n %d", y=z);
 
 
 
         
 	return 0;
 
-
 }
+
