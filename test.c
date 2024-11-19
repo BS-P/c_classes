79786da3bcf273435f47895d32c97a6ac0b3e6a6
Your Name <you@example.com>
2024-11-19 09:35:00 +0530

    unit place digit

--- test.c
+++ test.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
+int main ()
+{
+	int x ;
+	printf("please enter a num\n");
+	scanf ("%d",&x);
+	if (x>0)
+	{
+		printf("num is positive\n");
+	}
+	else
+	{
+        printf ("error");
+	}
+	
+	int y = x%10;
+	printf("unit place digit is: %d\n", y);
 
-int main () 
-{	
-	int x=3, y=5, z;
-        printf("\n %d", z=x );
-	printf("\n %d", x=y);
-	printf("\n %d", y=z);
-
-
-
-        
-	return 0;
-
+return 0 ;
 }
-
