e6b2f0cf00009b3be406d14b56502729dda194b1
Your Name <you@example.com>
2024-11-19 09:14:11 +0530

    positive number

---
+++ positive.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int x ;
+	printf("please enter a num\n");
+	scanf ("%d",&x);
+	if (x>0)
+	{
+		printf("%d/n",x);
+	}
+	else
+	{
+        printf ("error");
+	}
+
+return 0 ;
+ 
+
+}
+
+
