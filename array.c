567233862f486e8e102fab964a3377128f28d22d
Your Name <you@example.com>
2024-11-27 09:46:31 +0530

    Array

--- array.c
+++ array.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 int main ()
 {
-	int a;
-       printf("enter the value of element\n");
-        scanf("%d",&a[i]);
-       for(i=0;i<5;i++)
+	int a[5]={2,8,5,4,1};
+      
+      
+       for(int i=0;i<4;i++)
        {  
-       if (a[i]>a[a+1])
+       if (a[i]>a[i+1])
        {
-	int temp = a[i];       
-         a[i]=a[i+1];
-	a[i+1]= temp;
-       printf("value of element is:%d\n",a[i]);
-    
+	 int temp=a[i];
+	 a[i]=a[i+1];
+	 a[i+1]=temp;
+	 
        }
        }
-
+        
+       for (int i=0;i<5;i++)
+       {
+	printf("value of array is: %d\n",a[i]);       
+      
+       }   
       
 
 return 0;
