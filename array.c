049c9fd31b0721d24ee49fbdaa89c42f06d399c2
Your Name <you@example.com>
2024-11-29 10:03:52 +0530

    array swapping

--- array.c
+++ array.c
@@ -3,7 +3,7 @@ int main ()
 {
 	int a[5]={2,8,5,4,1};
         
-
+/*
 
        for(int i=0;i<4;i++)
        {
@@ -15,36 +15,26 @@ int main ()
 
        }  
        }
+*/
 
-       for (int i=0;i<5;i++)
-       {
-	printf("value of array is: %d\n",a[i]);
-
-       }
-
-       for (int j=0;j<5;j++)
-       {
-      for (int i=j;i<4;i++)
+      for (int j=0;j<4;j++)
+      {
+      for (int i=j;i<5;i++)
       {
 	      if (a[j]>a[i])
 	         {
-			int temp=a[j];
-		        a[j]=a[i];
-			a[i]=temp;
+			int temp=a[i];
+		        a[i]=a[j];
+			a[j]=temp;
 		 }
       }
+      }
 
-      for(int i=1;i<5;i++)
+      for(int i=0;i<5;i++)
       {
 	printf("value of array is: %d\n",a[i]);
       }
-       
-       }
-
-    
-
-
-      
+        
 
 
 return 0;
